8c6f19b92f628d5357f60c35f94300c0ffd469e6
edsant1 <edwins10329@gmail.com>
2016-08-15 15:03:32 -0600

    fixed local host issue

--- public/javascripts/notes.js
+++ public/javascripts/notes.js
@@ -3,6 +3,48 @@ $(document).ready(function() {
 	$('#add_note').onSubmit( function(e) {
 		e.preventDefault();
 
-		
-	})
+		var title = $(this).children('input').val();
+
+		$.ajax({
+			url: '/notes',
+			type: 'POST',
+			data: { title: title },
+			datatype: 'JSON'
+		}).done( function(data) {
+			$('#add_note input').val();
+			getAllNotes();
+		}).fail( function(err) {
+			console.log(err);
+		});
+	});
+
+	function getAllNotes() {
+		$.ajax({
+			url: '/notes',
+			type: 'GET',
+			datatype: 'JSON'
+		}).done( function(data) {
+			updateNoteList(data);
+		}).fail( function(err) {
+			console.log(err);
+		});
+	}
+
+	function updateNoteList(notes) {
+		var list = $('#note_list');
+		list.empty();
+
+		notes.forEach( function(notes) {	
+			$.ajax({
+				url: '/notes/note_template',
+				type: 'POST',
+				data: { id: note._id, title: note.title , complete: note.complete }
+				datatype: 'HTML'
+			}).done ( function(data) {
+				list.append(data);
+			})
+		})
+	}
+
+	getAllNotes();
 })
